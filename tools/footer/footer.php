4f1618a8c8dda5242ba183277c375697ecb8afcd
eisuqe <e.washiya@icloud.com>
2024-12-11 10:10:29 +0900

    初回コミット

---
+++ tools/footer/footer.php
@@ -0,0 +1,3 @@
+<div id="footer">
+    <a class="f1">©︎ 2024 Trippi All rights reserved</a>
+</div>
---
+++ trip-list.php
@@ -0,0 +1,5 @@
+<div class="trips">
+    <a href="#" class="trip-list-icon">
+    </a>
+    <a class="trip-list-text">demo</a>
+</div>
