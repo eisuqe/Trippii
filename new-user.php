aeb7b80159b14fa332cd151c515a6dbc62c43abb
senda293 <ryousuke169@icloud.com>
2024-12-16 14:46:07 +0900

    Update new-user.php

--- new-user.php
+++ new-user.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['register_error'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tranner</title>
+    <title>Trippii-NewUser</title>
     <link rel="stylesheet" href="./css/style.css">
     <?php include './tools/heads/head-link.php'; ?>
 </head>
@@ -24,21 +24,21 @@ if (isset($_SESSION['register_error'])) {
         <div class="new-user-main">
             <div class="new-user-main-txt">
                 <p1>新規会員登録</p1>
-                <p>Ma-ah!Ma-ah!Ma-ah!Ma-ah!Ma-ah!Ma-ah!</p>
+                <p>新しいアカウントの情報を設定してください</p>
             </div>
             <div class="new-user-main-form">
                 <?php if ($error): ?>
                     <p style="color: red; font-size: 15px;"><?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8'); ?></p>
                 <?php endif; ?>
                 <form action="register_process.php" method="POST">
-                    <label for="email">e-Mail</label><br>
+                    <label for="email">メールアドレス</label><br>
                     <input type="email" name="email" id="email" required>
                     <br>
-                    <label for="password">password</label><br>
+                    <label for="password">パスワード</label><br>
                     <input type="password" name="password" id="password" required>
                     <br>
                     <div class="new-user-main-form-button">
-                        <button type="submit">Create</button>
+                        <button type="submit">登録してログイン</button>
                     </div>
                 </form>
             </div>
