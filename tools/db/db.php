c55e834fe44d8f1d703e6744bbbe0e1a062d6d33
eisuqe <e.washiya@icloud.com>
2024-12-17 14:48:11 +0900

    Update db.php

--- tools/db/db.php
+++ tools/db/db.php
@@ -1,7 +1,8 @@
 <?php
 // データベース接続設定
 try {
-    $dsn = "pgsql:host=dpg-ctdofcaj1k6c73ds3pdg-a.singapore-postgres.render.com;port=5432;dbname=db_trippii;user=root;password=6vyi646onyQuRuxiQvgVm8DZDzjPI64e";
+    // SSL接続を有効にするためにsslmode=requireを追加
+    $dsn = "pgsql:host=dpg-ctdofcaj1k6c73ds3pdg-a.singapore-postgres.render.com;port=5432;dbname=db_trippii;user=root;password=6vyi646onyQuRuxiQvgVm8DZDzjPI64e;sslmode=require";
     $pdo = new PDO($dsn); // $user と $password は不要
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
