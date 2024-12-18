40f6216bf25a103ca287d046592e4181641c4b16
eisuqe <e.washiya@icloud.com>
2024-12-18 10:10:47 +0900

    q

--- tools/db/db.php
+++ tools/db/db.php
@@ -1,10 +1,16 @@
 <?php
 // データベース接続設定
 try {
-    // sslmode=prefer を使用してみる
-    $dsn = "mysql:host=dbtrippii.cdmms6ic0jg5.us-west-2.rds.amazonaws.com;port=3306;dbname=trippii;user=root;password=eaglerock70;";
-    $pdo = new PDO($dsn);
+    $dsn = "mysql:host=dbtrippii.cdmms6ic0jg5.us-west-2.rds.amazonaws.com;port=3306;dbname=trippii;";
+    $username = "root"; // AWS RDS MySQLのユーザー名
+    $password = "eaglerock70"; // AWS RDS MySQLのパスワード
+
+    // PDOインスタンス作成
+    $pdo = new PDO($dsn, $username, $password);
+
+    // エラーモードを例外に設定
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("データベース接続失敗: " . $e->getMessage());
 }
