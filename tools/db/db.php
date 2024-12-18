50859f71180863079408eadc1857d3a5709f5fb5
eisuqe <e.washiya@icloud.com>
2024-12-18 10:29:10 +0900

    Update db.php

--- tools/db/db.php
+++ tools/db/db.php
@@ -1,12 +1,12 @@
 <?php
 // データベース接続設定
 try {
-$dsn = "mysql:host=dbtrippii.cdmms6ic0jg5.us-west-2.rds.amazonaws.com;port=3306;dbname=trippii;charset=utf8mb4;";
-$options = [
-    PDO::MYSQL_ATTR_SSL_CA => '/path/to/aws/rds-combined-ca-bundle.pem',
-];
-$pdo = new PDO($dsn, 'root', 'eaglerock70', $options);
+    $dsn = "mysql:host=dbtrippii.cdmms6ic0jg5.us-west-2.rds.amazonaws.com;port=3306;dbname=trippii;";
+    $username = "root"; // AWS RDS MySQLのユーザー名
+    $password = "eaglerock70"; // AWS RDS MySQLのパスワード
 
+    // PDOインスタンス作成
+    $pdo = new PDO($dsn, $username, $password);
 
     // エラーモードを例外に設定
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
